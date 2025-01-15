365eaeefd6a45eae4b4cef73213aa0df83285d1a
Raissa Micheline IMPUHWE MANZI <rimpuhwemanzi@gmail.com>
2025-01-16 00:04:46 +0200

    updated lines

--- script.js
+++ script.js
@@ -1,40 +1,50 @@
 const inputText = document.getElementById("input-text");
-const dueDate = document.getElementById("due-date");
 const description = document.getElementById("description");
+const dueDate = document.getElementById("due-date");
 const listContainer = document.getElementById("task-list-items");
 const addTaskBtn = document.getElementById("add-task");
+const resetBtn = document.getElementById('reset-btn');
 
 function addTask() {
   if (inputText.value === "") {
-    alert(" Add atleast one work to be done ");
+    alert("please add atleast one Task , to continue");
   } else {
+    
     const tr = document.createElement("tr");
     const title = document.createElement("td");
     title.innerHTML = inputText.value;
     tr.appendChild(title);
 
-    const description = document.createElement("td");
-    description.innerHTML = description.value;
-    tr.appendChild(description);
+    const descriptionData = document.createElement("td");
+    descriptionData.innerHTML = description.innerHTML;
+    tr.appendChild(descriptionData);
 
-    const due = document.createElement("td");
-    due.innerHTML = dueDate.value;
-    tr.appendChild(due);
+    const dueDateData = document.createElement("td");
+    dueDateData.innerHTML = dueDate.value;
+    tr.appendChild(dueDateData);
+
+    const div = document.createElement("div");
     const span = document.createElement("span");
-    span.innerHTML = "\u00d7";
-    tr.appendChild(span);
+    const arr = ["edit", "delete"];
+    arr.forEach(
+      (text) =>
+        span.innerHTML += `<button id="${text}-button">${text}</button>`
+    );
+    div.appendChild(span);
+    tr.appendChild(div);
 
     listContainer.appendChild(tr);
+    resetBtn.style.display = "block";
   }
   inputText.value = "";
-  dueDate.value = "";
   description.value = "";
-  sortTasksByDueDate()
+  dueDate.value = "";
+  sortOutDatesByDue();
   save();
 }
 
-function sortTasksByDueDate() {
-  const rows = Array.from(listContainer.querySelectorAll("tr"));
+function sortOutDatesByDue(){
+  const rows = Array.from(listContainer.querySelectorAll('tr'));
   rows.sort((a, b) => {
     const dateA = new Date(a.cells[2].innerText);
     const dateB = new Date(b.cells[2].innerText);
@@ -42,37 +52,16 @@ function sortTasksByDueDate() {
   });
   rows.forEach((row) => listContainer.appendChild(row));
 }
-function editTask(tr){
-  const title = tr.cells[0].innerText
-  const description = tr.cells[1].innerText
-  const dueDate = tr.cells[2].innerText
-  inputText.value = title
-  description.value = description
-  dueDate.value = dueDate
-  save()
-}
-function save() {
+
+function save(){
   localStorage.setItem("data", listContainer.innerHTML);
 }
 function showData(){
-    listContainer.innerHTML = localStorage.getItem('data');
-
+  listContainer.innerHTML = localStorage.getItem("data");
 }
-showData();
-listContainer.addEventListener(
-  "click",
-  (e) => {
-    if (e.target.tagName === "TD") {
-      const tr = e.target.parentElement;
-      tr.classList.toggle("checked");
-      save();
-    } else if (e.target.tagName === "SPAN") {
-      e.target.parentElement.remove();
-      save();
-    }
-  },
-  false
-);
-
-//sorting due dates
-addTaskBtn.addEventListener("click", addTask);
+showData()
+addTaskBtn.addEventListener('click', addTask)
+resetBtn.addEventListener('click',()=>{
+  localStorage.removeItem('data');
+  listContainer.innerHTML = '';
+})
